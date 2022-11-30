5810d815de5869c5f8ca5370a33bdd1b1096ba63
SRS <stian.schikora@nav.no>
2022-11-30 10:47:08 +0100

    Video player cmp

--- force-app/main/default/lwc/nksVideoPlayer/nksVideoPlayer.js
+++ force-app/main/default/lwc/nksVideoPlayer/nksVideoPlayer.js
@@ -1,9 +1,12 @@
 import { LightningElement, wire } from 'lwc';
 import { CurrentPageReference } from 'lightning/navigation';
+import getVideoSrc from '@salesforce/apex/NKS_VideoPlayerCtrl.getVideoUrl';
 
 export default class NksVideoPlayer extends LightningElement {
     // Declare the currentPageReference variable in order to track it
     currentPageReference;
+    fileRef;
+    isLoading = false;
 
     @wire(CurrentPageReference)
     setCurrentPageReference(currentPageReference) {
@@ -13,5 +16,17 @@ export default class NksVideoPlayer extends LightningElement {
 
     getVideoReference() {
         //Get reference from parameters in the pagereference
+        this.isLoading = true;
+        let currentUrl = this.currentPageReference;
+        const reference = str.substring(currentUrl.lastIndexOf('/') + 1);
+
+        getVideoSrc({ videoRef: reference })
+            .then(videoSrc => {
+                this.fileRef = videoSrc;
+            })
+            .catch(error => {})
+            .finally(() => {
+                this.isLoading = false;
+            });
     }
 }
