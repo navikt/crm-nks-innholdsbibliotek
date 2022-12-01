4219c7171b7583bb13d6dd38bf4a24f0b34b8c7c
SRS <stian.schikora@nav.no>
2022-12-01 14:44:25 +0100

    Support view inside Salesforce

--- force-app/main/default/aura/nksVideoPlayerAura/nksVideoPlayerAuraHelper.js
+++ force-app/main/default/aura/nksVideoPlayerAura/nksVideoPlayerAuraHelper.js
@@ -1,11 +1,16 @@
 ({
     getVideoId: function(component) {
-        const queryUrl = decodeURIComponent(window.location.search.substring(1));
-        const videoId = queryUrl.split('=')[1]; //query should be on format key={videoId}
-        let videoSrc = window.location.origin + '/sfsites/c/sfc/servlet.shepherd/document/download/' + videoId;
-
-        component.set('v.videoUrl', videoSrc);
-
+        const recordId = component.get('v.recordId');
+        let videoSrc;
+        if (recordId) {
+            //Displaying inside Salesforce
+            videoSrc = window.location.origin + '/sfc/servlet.shepherd/document/download/' + recordId;
+        } else {
+            //In community
+            const queryUrl = decodeURIComponent(window.location.search.substring(1));
+            const videoId = queryUrl.split('=')[1]; //query should be on format key={videoId}
+            videoSrc = window.location.origin + '/sfsites/c/sfc/servlet.shepherd/document/download/' + videoId;
+        }
         const videoPlayer =
             '<video width="100%" controls controlsList="nodownload">><source src="' +
             videoSrc +
