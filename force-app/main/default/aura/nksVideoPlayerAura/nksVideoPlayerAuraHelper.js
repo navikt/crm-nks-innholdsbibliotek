be55c594bdeacff81a1317834d8bbdc0e69ff2d0
SRS <stian.schikora@nav.no>
2022-12-01 13:00:25 +0100

    Creating video player using unescaped html

--- force-app/main/default/aura/nksVideoPlayerAura/nksVideoPlayerAuraHelper.js
+++ force-app/main/default/aura/nksVideoPlayerAura/nksVideoPlayerAuraHelper.js
@@ -5,5 +5,11 @@
         let videoSrc = window.location.origin + '/sfsites/c/sfc/servlet.shepherd/document/download/' + videoId;
 
         component.set('v.videoUrl', videoSrc);
+
+        const videoPlayer =
+            '<video width="100%" controls controlsList="nodownload">><source src="' +
+            videoSrc +
+            '" type="video/mp4" /></video>';
+        component.set('v.videoPlayer', videoPlayer);
     }
 });
