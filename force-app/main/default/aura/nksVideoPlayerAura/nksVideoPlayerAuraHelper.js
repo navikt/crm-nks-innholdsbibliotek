f0d04e7078f9cc6266c90c9a60ca2145c25eaa08
SRS <stian.schikora@nav.no>
2022-12-01 10:31:18 +0100

    Player remove apex ctrl

--- force-app/main/default/aura/nksVideoPlayerAura/nksVideoPlayerAuraHelper.js
+++ force-app/main/default/aura/nksVideoPlayerAura/nksVideoPlayerAuraHelper.js
@@ -1,27 +1,9 @@
 ({
     getVideoId: function(component) {
-        const url = window.location.href;
-        const videoId = url.substring(url.lastIndexOf('/') + 1);
+        const queryUrl = decodeURIComponent(window.location.search.substring(1));
+        const videoId = queryUrl.split('=')[1]; //query should be on format key={videoId}
+        let videoSrc = window.location.origin + '/sfsites/c/sfc/servlet.shepherd/document/download/' + videoId;
 
-        let getVideoAction = cmp.get('c.serverEcho');
-        getVideoAction.setParams({ firstName: cmp.get('v.firstName') });
-
-        // Create a callback that is executed after
-        // the server-side action returns
-        getVideoAction.setCallback(this, function(response) {
-            const state = response.getState();
-            if (state !== 'SUCCESS') {
-                component.set('v.error', true);
-                return;
-            } else {
-                const videoUrl = response.getReturnValue();
-                component.set('v.videoUrl', videoUrl);
-            }
-        });
-
-        $A.enqueueAction(getVideoAction);
-
-        console.log('VIDEO ID IS: ' + videoId);
-        component.set('v.videoId', videoId);
+        component.set('v.videoUrl', videoSrc);
     }
 });
