4ca0dcbe010d681030b1d4145765d89c3631c611
SRS <stian.schikora@nav.no>
2022-12-02 12:22:37 +0100

    Video player width and centering

--- force-app/main/default/aura/nksVideoPlayerAura/nksVideoPlayerAuraHelper.js
+++ force-app/main/default/aura/nksVideoPlayerAura/nksVideoPlayerAuraHelper.js
@@ -12,7 +12,7 @@
             videoSrc = window.location.origin + '/sfsites/c/sfc/servlet.shepherd/document/download/' + videoId;
         }
         const videoPlayer =
-            '<video width="100%" controls controlsList="nodownload">><source src="' +
+            '<video width="80%" controls controlsList="nodownload">><source src="' +
             videoSrc +
             '" type="video/mp4" /></video>';
         component.set('v.videoPlayer', videoPlayer);
