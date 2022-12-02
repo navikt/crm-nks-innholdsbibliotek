af619986f8aadddedb377dc126cdec28291b9f68
SRS <stian.schikora@nav.no>
2022-12-02 13:56:53 +0100

    Lib link cmp with copy function

--- force-app/main/default/lwc/nksVideoLibLinker/nksVideoLibLinker.js
+++ force-app/main/default/lwc/nksVideoLibLinker/nksVideoLibLinker.js
@@ -1,5 +1,6 @@
 import { LightningElement, api } from 'lwc';
 import getExperienceUrl from '@salesforce/apex/NKS_VideoPlayerCtrl.getLibraryUrl';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class NksVideoLibLinker extends LightningElement {
     @api recordId;
@@ -14,7 +15,32 @@ export default class NksVideoLibLinker extends LightningElement {
             .catch(error => {});
     }
 
+    handleCopy(event) {
+        let copyValue = event.currentTarget.value;
+        let hiddenInput = document.createElement('input');
+        hiddenInput.value = copyValue;
+        document.body.appendChild(hiddenInput);
+        hiddenInput.focus();
+        hiddenInput.select();
+        try {
+            var successful = document.execCommand('copy');
+            this.showCopyToast(successful ? 'success' : 'error');
+        } catch (error) {
+            this.showCopyToast('error');
+        }
+        document.body.removeChild(hiddenInput);
+    }
+
+    showCopyToast(status) {
+        const evt = new ShowToastEvent({
+            message: status === 'success' ? 'kopiert til utklippstavlen.' : 'Kunne ikke kopiere',
+            variant: status,
+            mode: 'pester'
+        });
+        this.dispatchEvent(evt);
+    }
+
     get libraryUrl() {
-        return this.experienceUrl ? this.experienceUrl + this.recordId : null;
+        return this.experienceUrl ? this.experienceUrl + '?videoId=' + this.recordId : null;
     }
 }
