b6cdf3a48591bf9f9ac3a3a34941a5abc98da5fb
eirikfladby <eirik.fladby@gmail.com>
2023-04-20 20:16:02 +0200

    hide video and embed link if not mp4

--- force-app/main/default/lwc/nksVideoDetails/nksVideoDetails.js
+++ force-app/main/default/lwc/nksVideoDetails/nksVideoDetails.js
@@ -1,5 +1,17 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import getFileType from '@salesforce/apex/NKS_VideoPlayerCtrl.checkFileType';
 
 export default class NksVideoDetails extends LightningElement {
     @api recordId;
+    fileType = false;
+
+    @wire(getFileType, { recordId: '$recordId'})
+    wiredGetFileType(result) {
+        console.log('hello');
+        if (result.error) {
+            console.log(result.error);
+        } else if (result.data) {
+            this.fileType = result.data === 'mp4';
+        }
+    }
 }
