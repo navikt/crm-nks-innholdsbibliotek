70bf33740ca7abec9c7b50aade3a0f3b000873b9
eirikfladby <eirik.fladby@gmail.com>
2023-06-19 13:23:31 +0200

    layout changes

--- force-app/main/default/lwc/nksVideoDetails/nksVideoDetails.js
+++ force-app/main/default/lwc/nksVideoDetails/nksVideoDetails.js
@@ -4,13 +4,16 @@ import getFileType from '@salesforce/apex/NKS_VideoPlayerCtrl.checkFileType';
 export default class NksVideoDetails extends LightningElement {
     @api recordId;
     isFileTypeMp4 = false;
-
+    isSubtitleFile = false;
+    videoFileTypes = ['mp4', 'mov', 'avi', 'wmv', 'flv', 'avchd'];
+    subtitleFileTypes = ['vtt']; // Does not support .srt at the moment
     @wire(getFileType, { recordId: '$recordId'})
     wiredGetFileType(result) {
         if (result.error) {
             console.log(result.error);
         } else if (result.data) {
-            this.isFileTypeMp4 = result.data === 'mp4';
+            this.isFileTypeMp4 = this.videoFileTypes.includes(result.data);
+            this.isSubtitleFile = this.subtitleFileTypes.includes(result.data);
         }
     }
 }
