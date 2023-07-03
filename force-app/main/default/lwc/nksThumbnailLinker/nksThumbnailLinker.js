4d815b4b675513b2acc2f134e242d071eeee07cf
eirikfladby <eirik.fladby@gmail.com>
2023-07-03 12:43:00 +0200

    added thumbnail linker component

---
+++ force-app/main/default/lwc/nksThumbnailLinker/nksThumbnailLinker.js
@@ -0,0 +1,44 @@
+import { LightningElement, api } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { isVideoFile, isSubtitleFile } from 'c/utils';
+import label from 'c/utils';
+
+export default class NksThumbnailLinker extends LightningElement {
+    @api recordId;
+    @api filetype;
+    label = label;
+
+    handleCopy() {
+        let copyValue = this.showThumbnailLink;
+        let hiddenInput = document.createElement('input');
+        hiddenInput.value = copyValue;
+        document.body.appendChild(hiddenInput);
+        hiddenInput.focus();
+        hiddenInput.select();
+        try {
+            document.execCommand('copy');
+        } catch (error) {
+            this.showCopyToast('error');
+        }
+        document.body.removeChild(hiddenInput);
+        this.template.querySelector('[data-id="thumbnail-copy-button"]').focus(); // Put focus back on copy button - for UU
+    }
+
+    showCopyToast(status) {
+        const evt = new ShowToastEvent({
+            message: label.COPY_FAIL,
+            variant: status,
+            mode: 'pester'
+        });
+        this.dispatchEvent(evt);
+    }
+
+    // Easier than checking all the image variants
+    get isThumbnailFile() { 
+        return !isSubtitleFile(this.filetype) && !isVideoFile(this.filetype);
+    }
+    
+    get showThumbnailLink() {
+        return window.location.origin + '/sfc/servlet.shepherd/document/download/' + this.recordId;
+    }
+}
